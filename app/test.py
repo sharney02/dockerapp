b45f4ca09ec2ad5a46d3e2e94defeceda37641f8
Sé Harney <seharney@ses-macbook-pro.home>
2022-02-14 16:33:15 +0000

    adding logging

--- app/test.py
+++ app/test.py
@@ -15,6 +15,7 @@ class TestDockerapp(unittest.TestCase):
     def test_load_value(self):
         self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
         response = self.app.post('/', data=dict(submit='load', key='2'))
+        print('adding extra line')
         assert response.status_code == 200
         assert b'2' in response.data
         assert b'two' in response.data
